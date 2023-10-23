72b2908a3ee2996a204b10f4c3347576d131a08a
samirvithlani <samir.vithlani83955@gmail.com>
2023-10-23 18:39:23 +0530

    m

--- controllers/EmployeeController.js
+++ controllers/EmployeeController.js
@@ -1,44 +1,50 @@
-const employeeModel = require("../models/EmployeeModel.js");
+const employeeModel = require('../models/EmployeeModel.js');
 
-const addEmployee = (req, res) => {};
+const addEmployee = (req,res)=>{
 
-const getAllEmployees = (req, res) => {
-  employeeModel
-    .find()
-    .then((employees) => {
-      res.status(200).json({
-        data: employees,
-        message: "employees fetched successfully",
-      });
+
+}
+
+const getAllEmployees = (req,res)=>{
+
+
+    employeeModel.find().then((employees)=>{
+
+        res.status(200).json({
+            data: employees,
+            message:"employees fetched successfully"
+        })
+
+
+    }).catch((err)=>{
+        res.staus(500).json({
+            message: err.message
+        })
     })
-    .catch((err) => {
-      res.staus(500).json({
-        message: err.message,
-      });
-    });
-};
 
-const getEmployeeById = (req, res) => {
-  //params --> req-->
-  const id = req.params.id; //id
+}
 
-  //employeeModel.find({_id:id})
-  employeeModel
-    .findById(id)
-    .then((employee) => {
-      res.status(200).json({
-        data: employee,
-        message: "employee fetched successfully",
-      });
+const getEmployeeById = (req,res)=>{
+
+    //params --> req-->
+    const id = req.params.id; //id 
+
+    //employeeModel.find({_id:id})
+    employeeModel.findById(id).then((employee)=>{
+        res.status(200).json({
+            data: employee,
+            message:"employee fetched successfully"
+        })
+    }).catch((err)=>{
+        res.status(500).json({
+            message: err.message
+        })
     })
-    .catch((err) => {
-      res.status(500).json({
-        message: err.message,
-      });
-    });
-};
+
+}
 
 // const getEmployeeByname = (req,res)=>{
+
 
 //     const name = req.params.name;
 
@@ -53,36 +59,44 @@ const getEmployeeById = (req, res) => {
 //         })
 //     })
 
+
+
 // }
 
-const getEmployeeByname = (req, res) => {
-  const name = req.params.name;
+const getEmployeeByname = (req,res)=>{
 
-  employeeModel
-    .find({ name: name })
-    .then((employee) => {
-      if (employee && employee.length > 0) {
-        res.status(200).json({
-          data: employee,
-          message: "employee fetched successfully",
-        });
-      } else {
-        res.status(404).json({
-          data: [],
-          message: "employee not found",
-        });
-      }
+
+    const name = req.params.name;
+
+    employeeModel.find({name:name}).then((employee)=>{
+        if(employee && employee.length>0){
+            res.status(200).json({
+                data: employee,
+                message:"employee fetched successfully",
+                
+            })
+        }
+            else{
+                res.status(404).json({
+                data: [],
+                message:"employee not found",
+                })
+            }
+        
+    }).catch((err)=>{
+        res.status(500).json({
+            message: err.message
+        })
     })
-    .catch((err) => {
-      res.status(500).json({
-        message: err.message,
-      });
-    });
-};
+
+
+
+}
+
 
 module.exports = {
-  addEmployee,
-  getAllEmployees,
-  getEmployeeById,
-  getEmployeeByname,
-};
+    addEmployee,
+    getAllEmployees,
+    getEmployeeById,
+    getEmployeeByname
+}
